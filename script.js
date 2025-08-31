d1ba2fcec3b3440152555cb0850145c9c51a39df
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-08-31 13:48:19 +0330

    js part1

---
+++ script.js
@@ -0,0 +1,16 @@
+document.querySelector("#push").onclick = function () {
+  if (document.querySelector("#newtask input").value.length == 0) {
+    alert("Please Enter A Task");
+  } else {
+    document.querySelector("#tasks").innerHTML += `
+        <div class="task">
+          <span id="taskname">
+          ${document.querySelector("#newtask input").value}
+          </span>
+          <button class"delete">
+          <i class="far fa-trash-alt"></i>
+          </button>
+        </div>
+        `;
+  }
+};
